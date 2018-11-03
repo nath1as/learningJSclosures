1cdbc98a1248b7dd2cecd15ec99d7c85406f5040
nathias <n@th1.as>
2018-11-03 11:55:23 +0100

    second done

---
+++ ppp2.js
@@ -0,0 +1,15 @@
+var total = 0;
+
+function add(num) {
+  total += num;
+  console.log(total);
+}
+function subtract(num) {
+  total -= num;
+  console.log(total);
+}
+
+add(1);
+add(42);
+subtract(39);
+add(6);
