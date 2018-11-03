011962e979836baeb0b357e9e60d84d7b26bda65
nathias <n@th1.as>
2018-11-03 18:03:11 +0100

    gc4

---
+++ gc4.js
@@ -0,0 +1,12 @@
+function makeEvenCounter() {
+  var index = 0;
+  return function() {
+    return index += 2;
+  };
+}
+
+var evenCounter = makeEvenCounter();
+
+// is 0 eligible for GC here?
+// no, evenCounter is a closure
+// more code
